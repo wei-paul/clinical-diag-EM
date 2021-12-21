fd6dd29bb24c78cd7381b1514f6056329659b1aa
Paul <paul.wei607@gmail.com>
2021-12-20 23:14:11 -0800

    deletes react templates and brings in EM api

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="elementor-widget-container">
+      <iframe src="https://dodxtx.shinyapps.io/EMSC/">     
+    </iframe>
+    </div>
+  );
+}
+
+export default App;
